b5271ba1a4417e50d4b22b1f548fc8735f54ef77
Gerardo Sanchez <gschz.git@gmail.com>
2026-05-17 12:38:09 -0700

    chore: update module exports, types, and test configurations

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
     include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx'],
     clearMocks: true,
     restoreMocks: true,
+    setupFiles: ['./tests/setup.ts'],
   },
 });
